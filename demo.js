d5980ec75471dd181f796c53db0dd7a1866c1bad
vikas9620 <vikassingh9620@gmail.com>
2023-01-29 14:00:36 +0530

    initial commit

---
+++ demo.js
@@ -0,0 +1,18 @@
+console.log(document.domain)
+console.log(document.URL)
+console.log(document.title)
+
+
+console.log(document.doctype)
+console.log(document.head)
+console.log(document.body)
+console.log(document.all)
+//console.log(document.all[10])
+//document.all[10].textContent = "hello"
+console.log(document.forms)
+console.log(document.links)
+
+//console.log(document.getElementById('header-title'))
+var header = document.getElementById('header-title')
+console.log(header-title)
+
