014a89e7e897f03f30e03845e4d30ff292b1d431
vikas9620 <vikassingh9620@gmail.com>
2023-02-02 19:21:03 +0530

    initial commit

--- demo.js
+++ demo.js
@@ -20,7 +20,7 @@ var header = document.getElementById('title')
 console.log(header)
 header.style.fontWeight = "bold"
 header.style.color = "lightgreen"
-*/
+
 
 var li = document.getElementsByTagName("li")
 li[4].style.color = "yellow"
@@ -31,5 +31,17 @@ for (var i=0; i<items.length; i++){
     items[i].style.color = "lightgreen"
 }
 
+*/
+var secondItem = document.querySelector(".list-group-item:nth-child(2)")
+secondItem.style.backgroundColor = "green"
+var thirdItem = document.querySelector(".list-group-item:nth-child(3)")
+thirdItem.style.display = "none"
 
+
+var items = document.querySelectorAll("li")
+items[1].style.color ="green"
+var odd = document.querySelectorAll("li:nth-child(odd)")
+for(let i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor = "green"
+}
 
